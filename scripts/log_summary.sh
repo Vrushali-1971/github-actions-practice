574adc4121f74085994cecb54e693a793f5abe1d
Vrushali-1971 <vrushaliambore3908@gmail.com>
2026-04-26 04:51:05 +0000

    fix: modify script and add two jobs to test workflow

--- scripts/log_summary.sh
+++ scripts/log_summary.sh
@@ -3,17 +3,25 @@
 DIR="$1"
 
 #Argument Validation
-if [ -z "$1" ]; then
+if [ -z "$DIR" ]; then
 	echo "Please provide directory path"
 	exit 1
 fi
 
 # Directory Validation
-if [ ! -d "$1" ]; then
+if [ ! -d "$DIR" ]; then
 	echo "Directory does not exist"
 	exit 1
 fi
 log_count=$(find "$DIR" -name '*.log' | wc -l)
 total_errors=$(find "$DIR" -name "*.log" -exec grep -h  "ERROR" {} +  2>/dev/null | wc -l)
+
 echo "The log files: $log_count"
 echo "Total ERROR lines: $total_errors"
+
+
+if [ "$total_errors" -gt 0 ]; then
+	exit 1
+else 
+	exit 0
+fi
