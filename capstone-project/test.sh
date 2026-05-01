9dd7822b986545c8d88d8058e0082743923447c3
Vrushali-1971 <vrushaliambore3908@gmail.com>
2026-05-01 10:37:03 +0000

    update test.sh file syntax

--- capstone-project/test.sh
+++ capstone-project/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Running health check..."
-curl -f http://localhost:5000 && echo "Test Passed" || (echo "Test Failed" && exit 1)
+curl -f http://localhost:5000 > /dev/null && echo "Test Passed" || (echo "Test Failed" && exit 1)
